4cf13749f1ce7cc0325517bc984f72148f1925ae
mcacciano <michael.cacciano@gmail.com>
2020-05-19 16:03:41 -0400

    re styled login form to be mobile first

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Input = ({ label, className, ...restProps }) => {
   return (
     <label className="flex flex-col mb-4">
-      <span className="text-xs text-semibold pb-2">{label}</span>
+      <span className="text-sm text-semibold pb-2">{label}</span>
       <input
         className={`rounded px-2 py-1 bg-gray-100 border border-gray-300 focus:outline-none focus:bg-white ${className}`}
         {...restProps}
--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -9,9 +9,9 @@ import Button from '../../components/Button';
 import { signInWithGoogle } from '../../firebase/init';
 
 const LoginPage = () => {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
   const handleOnSubmit = e => {
     e.preventDefault();
   };
@@ -25,37 +25,55 @@ const LoginPage = () => {
 
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <div className="flex flex-row w-2/3 border border-black border-opacity-50 rounded">
-        <div className="w-1/2 relative">
+      <div className="flex flex-col justify-center items-center w-full h-full border border-black border-opacity-50 rounded relative">
+        <div className="w-full h-full left-0 top-0 absolute">
           <img
-            className="rounded rounded-tr-none rounded-br-none"
+            className="w-full h-full rounded rounded-tr-none rounded-br-none object-cover z-0 lg:object-center"
             src={SBImg1}
             alt="Shadowbringers left side of form"
           />
           <div className="absolute left-0 top-0 w-full h-full bg-black bg-opacity-25"></div>
         </div>
-        <div className="w-1/2 flex flex-col">
+        <div className="z-10 bg-white rounded border border-black border-opacity-75 w-3/4 max-w-screen-md">
+          <div className="flex flex-row justify-center align-center py-4 md:py-8">
+            <h1 className="text-3xl md:text-4xl">Welcome to LetsRaid</h1>
+          </div>
           <form
-            className="flex flex-col flex-grow justify-center p-6"
+            className="flex flex-col justify-center mx-auto p-6 md:w-3/4"
             onSubmit={handleOnSubmit}
           >
-            <Input onChange={handleEmail} type="email" label="Email" className="mb-4" />
-            <Input onChange={handlePassword} type="password" label="Password" className="mb-4" />
-            <div className="flex flex-row justify-between">
-              <Button onClick={handleOnClick}>Login</Button>
+            <Input
+              onChange={handleEmail}
+              type="email"
+              label="Email"
+              className="mb-4"
+            />
+            <Input
+              onChange={handlePassword}
+              type="password"
+              label="Password"
+              className="mb-4"
+            />
+            <div className="flex flex-col justify-between mb-4 md:flex-row">
+              <Button className="mb-2 md:mb-0" onClick={handleOnClick}>
+                Login
+              </Button>
               <Button onClick={signInWithGoogle} isGoogleSignIn>
                 Login With Google
               </Button>
             </div>
+            <div className="flex flex-row justify-center">
+              <p className="flex flex-col items-center md:flex-row text-sm py-2">
+                <span className="italic">Don't have an account yet?</span>{' '}
+                <Link
+                  to="/"
+                  className="pl-2 font-semibold text-purple-800 hover:border"
+                >
+                  Sign Up!
+                </Link>
+              </p>
+            </div>
           </form>
-          <div className="flex flex-row justify-center">
-            <p className="text-sm py-2">
-              <span className="italic">Don't have an account yet?</span>{' '}
-              <Link to="/" className="pl-2 font-semibold text-blue-500 hover:border">
-                Sign Up!
-              </Link>
-            </p>
-          </div>
         </div>
       </div>
     </div>
